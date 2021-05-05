820fb17d646029b7e4ce3f14918d2df4ec602636
Lynde Bayona <lynde.bayona@erfinderstore.com>
2021-05-05 12:00:40 +0800

    Initial commit.

---
+++ lib/main.dart
@@ -0,0 +1,204 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Flutter Connectivity'),
+          ),
+          body: Builder(builder: (BuildContext context) {
+            return OfflineBuilder(
+              connectivityBuilder: (BuildContext context,
+                  ConnectivityResult connectivity, Widget child) {
+                final bool connected = connectivity != ConnectivityResult.none;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    child,
+                    Positioned(
+                      left: 0.0,
+                      right: 0.0,
+                      height: 12.0,
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 300),
+                        color: connected ? null : Color(0xFFEE4400),
+                        child: connected
+                            ? null
+                            : Row(
+                                children: <Widget>[
+                                  Text(
+                                    "OFFLINE",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  SizedBox(
+                                    width: 8.0,
+                                  ),
+                                  SizedBox(
+                                    width: 12.0,
+                                    height: 12.0,
+                                    child: CircularProgressIndicator(
+                                      strokeWidth: 2.0,
+                                      valueColor: AlwaysStoppedAnimation<Color>(
+                                          Colors.white),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                      ),
+                    )
+                  ],
+                );
+              },
+              child: Center(
+                  child: Text("Check ONLINE/OFFLINE Connectivity Status")),
+            );
+          })),
+    );
+  }
+}
+
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key, this.title}) : super(key: key);
+
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+
+//   final String title;
+
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+
+//   void _incrementCounter() {
+//     setState(() {
+//       // This call to setState tells the Flutter framework that something has
+//       // changed in this State, which causes it to rerun the build method below
+//       // so that the display can reflect the updated values. If we changed
+//       // _counter without calling setState(), then the build method would not be
+//       // called again, and so nothing would appear to happen.
+//       _counter++;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Here we take the value from the MyHomePage object that was created by
+//         // the App.build method, and use it to set our appbar title.
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         // Center is a layout widget. It takes a single child and positions it
+//         // in the middle of the parent.
+//         child: Column(
+//           // Column is also a layout widget. It takes a list of children and
+//           // arranges them vertically. By default, it sizes itself to fit its
+//           // children horizontally, and tries to be as tall as its parent.
+//           //
+//           // Invoke "debug painting" (press "p" in the console, choose the
+//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
+//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+//           // to see the wireframe for each widget.
+//           //
+//           // Column has various properties to control how it sizes itself and
+//           // how it positions its children. Here we use mainAxisAlignment to
+//           // center the children vertically; the main axis here is the vertical
+//           // axis because Columns are vertical (the cross axis would be
+//           // horizontal).
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Text(
+//               '$_counter',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _incrementCounter,
+//         tooltip: 'Increment',
+//         child: Icon(Icons.add),
+//       ), // This trailing comma makes auto-formatting nicer for build methods.
+//     );
+//   }
+// }
+
+// class DemoPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return new Scaffold(
+//       appBar: new AppBar(
+//         title: new Text("Offline Demo"),
+//       ),
+//       body: OfflineBuilder(
+//         connectivityBuilder: (
+//           BuildContext context,
+//           ConnectivityResult connectivity,
+//           Widget child,
+//         ) {
+//           final bool connected = connectivity != ConnectivityResult.none;
+//           return new Stack(
+//             fit: StackFit.expand,
+//             children: [
+//               Positioned(
+//                 height: 24.0,
+//                 left: 0.0,
+//                 right: 0.0,
+//                 child: Container(
+//                   color: connected ? Color(0xFF00EE44) : Color(0xFFEE4400),
+//                   child: Center(
+//                     child: Text("${connected ? 'ONLINE' : 'OFFLINE'}"),
+//                   ),
+//                 ),
+//               ),
+//               Center(
+//                 child: new Text(
+//                   'Yay!',
+//                 ),
+//               ),
+//             ],
+//           );
+//         },
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Text(
+//               'There are no bottons to push :)',
+//             ),
+//             new Text(
+//               'Just turn off your internet.',
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
